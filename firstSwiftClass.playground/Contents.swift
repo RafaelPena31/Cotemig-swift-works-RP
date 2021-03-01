970e763c76b0591075d555a2256df023722a5fe9
RafaelPena31 <rafaelppena31@gmail.com>
2021-03-01 09:52:35 -0300

    First Swift Homework

---
+++ firstSwiftClass.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+// 1
+var maneiraCurta = 3
+var maneiraLonga:Int = 4
+
+// 2
+let placa = "abc-1234"
+let chassi = "178634hjshd273"
+let modelo = "Tesla Model"
+let cor = "Prata"
+let marca = "Tesla"
+let anoFabrica = "2020"
+let nPortas = 2
+let nRodas = 4
+var proprietario = "Rafael Pena"
+var valor = 50000
+var kilometragem = 25000
+
+// 3
+
+/* Utilizamos os comentarios para adicionarmos referencias, anotacoes, explicacoes, dentre diversas outras coisas em nosso codigo de maneira que na hora que o codigo seja executado, ele as ignore */
+
+// 4
+var numberOne:Int = 5
+var numberTwo:Double = 4.32
+var text:String = String(numberOne) + String(numberTwo)
+print(text)
+
+//5
+
+/*
+Type Safety: Maneira de se tipar uma variavel ao inicia-la e garantir que essa variavel nao recebera um valor de tipo diferente ao dela.
+*/
+
+/*
+Type Inference: Maneira de auto-deteccao de tipagem a partir do valor setado ao iniciar a variavel.
+*/
