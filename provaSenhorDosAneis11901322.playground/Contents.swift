b4e52f41a2a0792b8137f9dc6def07166c52ce91
RafaelPena31 <rafaelppena31@gmail.com>
2021-05-03 10:00:08 -0300

    Prova Trimestral - Primeiro trimeste - Tipo A

--- provaSenhorDosAneis11901322.playground/Contents.swift
+++ provaSenhorDosAneis11901322.playground/Contents.swift
@@ -6,7 +6,7 @@ AUTOR DO PROJETO
  Número de chamada: 46
  Nome completo: Rafael Augusto Pena Pereira Mesquita
  
- A avaliação foi realizada a partir dos meus conhecimentos adquiridos na aula de Desenvolvimento IOS e conhecimentos sobre programação adquiridos durante minha tragetória
+ A avaliação foi realizada a partir dos meus conhecimentos adquiridos na aula de Desenvolvimento IOS e conhecimentos sobre programação adquiridos durante minha tragetória profissional
 */
 
 // Constante de controle: quantidade de personagens
